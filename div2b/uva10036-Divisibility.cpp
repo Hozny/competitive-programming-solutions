ab84ee227f7d85f210614bec0470caa63b85ddbe
Yousof Hosny <yhosnysa@uwaterloo.ca>
2020-07-15 14:45:25 -0400

    delete exe files

---
+++ div2b/uva10036-Divisibility.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int mm; 
+    cin >> mm; 
+    for (int ti = 0; ti < mm; ++ti) { 
+        int N, K; 
+        cin >> N >> K; 
+        int a[N];
+        for (int i = 0; i < N; ++i) { 
+            cin >> a[i];
+        }
+  
+    }
+} 
